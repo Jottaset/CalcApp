18b52ecaf2844847ab1bc8b922b91e4a0b3212cb
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-02-28 21:41:25 -0400

    first commit

---
+++ CalcApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalcApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Somando(object sender, System.EventArgs e)
+        {
+            int n1 = 0, n2 = 0, n = 0;
+
+            n1 = Convert.ToInt32(Num_01.Text);
+
+            n2 = Convert.ToInt32(Num_02.Text);
+
+            n = n1 + n2;
+
+            label_1.Text = n.ToString();
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
